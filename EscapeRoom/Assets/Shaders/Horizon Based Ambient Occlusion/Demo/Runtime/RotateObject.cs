ca7d9a37311246ff26dd46cf0c5e89acfac09543
phonkriminal <phonkriminal@gmail.com>
2023-08-06 01:57:50 +0200

    Shader Updated
    Added Shaders and Shaders Editors

---
+++ EscapeRoom/Assets/Shaders/Horizon Based Ambient Occlusion/Demo/Runtime/RotateObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace HorizonBasedAmbientOcclusion
+{
+    public class RotateObject : MonoBehaviour
+    {
+        [Range(-50.0f, 50.0f)]
+        public float speed = 15.0f;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(speed * Time.deltaTime * Vector3.up, Space.World);
+        }
+    }
+}
