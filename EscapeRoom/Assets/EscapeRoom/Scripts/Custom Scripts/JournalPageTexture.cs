023b93d392c56552d41fbc4fd47d5976758b27e5
phonkriminal <phonkriminal@gmail.com>
2023-08-27 20:34:34 +0200

    Journal Scripts and Graphics Fixed

--- EscapeRoom/Assets/EscapeRoom/Scripts/Custom Scripts/JournalPageTexture.cs
+++ EscapeRoom/Assets/EscapeRoom/Scripts/Custom Scripts/JournalPageTexture.cs
@@ -8,7 +8,8 @@ public class JournalPageTexture : MonoBehaviour
     [SerializeField] private PageTexture[] pageTextures;
     [SerializeField] private string journalMenuName = "Journal";
     [SerializeField] private string journalElementName = "PageText";
-    [SerializeField] private string graphicElementName = "Texture";
+    [SerializeField] private string leftGraphicElementName = "Left Texture";
+    [SerializeField] private string rightGraphicElementName = "Right Texture";
     [SerializeField] private Texture2D emptyTexture;
 
     private void OnEnable()
@@ -27,7 +28,8 @@ public class JournalPageTexture : MonoBehaviour
     {
         if (menu.title == journalMenuName)
         {
-            UpdatePageTexture();
+            UpdateLeftPageTexture();
+            UpdateRigthPageTexture();
         }
     }
 
@@ -35,16 +37,20 @@ public class JournalPageTexture : MonoBehaviour
     {
         if (PlayerMenus.GetMenuWithName(journalMenuName).elements.Contains(_element))
         {
-            UpdatePageTexture();
+            UpdateLeftPageTexture();
+            UpdateRigthPageTexture();
         }
     }
 
-    private void UpdatePageTexture()
+    private void UpdateRigthPageTexture()
     {
         MenuJournal journal = PlayerMenus.GetElementWithName(journalMenuName, journalElementName) as MenuJournal;
         int pageIndex = journal.showPage - 1;
 
-        MenuGraphic graphic = PlayerMenus.GetElementWithName(journalMenuName, graphicElementName) as MenuGraphic;
+        pageIndex++;
+
+        MenuGraphic graphic = PlayerMenus.GetElementWithName(journalMenuName, rightGraphicElementName) as MenuGraphic;
+
         Texture2D texture = emptyTexture;
         if (pageIndex < journal.pages.Count)
         {
@@ -57,6 +63,28 @@ public class JournalPageTexture : MonoBehaviour
             graphic.graphic.texture = texture;
             graphic.graphic.ClearCache();
         }
+    }
+
+    private void UpdateLeftPageTexture()
+    {
+        MenuJournal journal = PlayerMenus.GetElementWithName(journalMenuName, journalElementName) as MenuJournal;
+        int pageIndex = journal.showPage - 1;
+
+        MenuGraphic graphic = PlayerMenus.GetElementWithName(journalMenuName, leftGraphicElementName) as MenuGraphic;
+
+        Texture2D texture = emptyTexture;
+        if (pageIndex < journal.pages.Count)
+        {
+            int pageID = journal.pages[pageIndex].lineID;
+            texture = GetTextureWithID(pageID);
+        }
+
+        if (texture)
+        {
+            graphic.graphic.texture = texture;
+            graphic.graphic.ClearCache();
+        }       
+
     }
 
     private Texture2D GetTextureWithID(int ID)
